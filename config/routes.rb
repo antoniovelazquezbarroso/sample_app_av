3201d6481dec22267acc7c7085c3114bc8143231
antonio <antonio@example.com>
2011-11-01 21:53:07 +0100

    Done with sign in. pag363

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleAppAv::Application.routes.draw do
 
   resources :users
-
+  resources :sessions, :only => [:new, :create, :destroy]
+    
   root :to => "pages#home"
 
   match '/signup', :to => 'users#new'
@@ -9,7 +10,12 @@ SampleAppAv::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
+  
+  match '/signup',  :to => 'users#new'
+  
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
 #  get "pages/home"
 
 #  get "pages/contact"
