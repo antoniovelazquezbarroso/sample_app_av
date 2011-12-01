fd74527051191db5d15994c62b94996e2a33fda0
antonio <antonio@example.com>
2011-12-01 22:42:47 +0100

    Added user microposts.MHartl pg458

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,10 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end  
 
   def self.authenticate(email, submitted_password)
       user = find_by_email(email)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleAppAv::Application.routes.draw do
 
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
     
   root :to => "pages#home"
 
